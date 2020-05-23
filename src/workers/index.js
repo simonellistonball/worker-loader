0111bfb1e3dea17032226383504418f9d0ca5f83
Simon Elliston Ball <simon@simonellistonball.com>
2020-05-23 14:12:34 +0100

    Added tiny-worker support

--- src/workers/index.js
+++ src/workers/index.js
@@ -20,8 +20,8 @@ const getWorker = (file, content, options) => {
       content
     )}, ${fallbackWorkerPath})`;
   }
-
-  return `new Worker(${publicWorkerPath})`;
+  
+  return `const Worker = require('tiny-worker'); new Worker(${publicWorkerPath})`;
 };
 
 export default getWorker;
